3e9221ad8454140ed515b9ee691fb1f7c05b5ebb
Paul Gathondu <aulgondu@gmail.com>
2023-06-08 18:47:53 -0700

    remove dev items from trade page"

--- src/components/ui/Tabs.jsx
+++ src/components/ui/Tabs.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import ProgressBar from "./ProgressBar";
-import Dropdown from "./Dropdown";
 import FlipCard from "./FlipCard";
 import OrderHistory from "../tables/OrderHistory";
 
@@ -64,56 +63,6 @@ function Tabs({ soldShares, totalShares, raisedAmount }) {
         >
           <div className="flex fle-col items-center">
             <OrderHistory />
-            <Dropdown
-              icon={
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-8 w-8  bg-tranparent"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <circle cx="10" cy="4" r="1.5" />
-                  <circle cx="10" cy="10" r="1.5" />
-                  <circle cx="10" cy="16" r="1.5" />
-                </svg>
-              }
-              menu={
-                <ul className="py-1">
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
-                      Pending Orders
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
-                      Recurring
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
-                      Completed
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-                    >
-                      Canceled
-                    </a>
-                  </li>
-                </ul>
-              }
-            />
           </div>
         </div>
         <div
@@ -165,21 +114,6 @@ Tabs.Header = ({ isTabActive, handleTabClick }) => (
         onClick={() => handleTabClick("orders")}
       >
         Orders
-      </button>
-    </li>
-    <li className="mr-2" role="presentation">
-      <button
-        className={`inline-block p-4 border-b-2 rounded-t-lg hover:text-blue-600 hover:border-gray-300 dark:hover:text ${
-          isTabActive("settings") ? "border-blue-600" : "border-transparent"
-        }`}
-        id="settings-tab"
-        type="button"
-        role="tab"
-        aria-controls="settings"
-        aria-selected={isTabActive("settings")}
-        onClick={() => handleTabClick("settings")}
-      >
-        Settings
       </button>
     </li>
   </ul>
