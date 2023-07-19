97fda8fed1b9360b0230f72fc459a20dfe1f22c8
Paul Gathondu <aulgondu@gmail.com>
2023-07-19 15:21:26 -0700

    setup prod api

--- src/api/django.js
+++ src/api/django.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // baseURL: 'https://tradingapp.up.railway.app/api/'
-  baseURL: 'http://127.0.0.1:8000/api/'
+  baseURL:  import.meta.env.DEV ? 'http://127.0.0.1:8000/api/' : 'https://tradingapp.up.railway.app/api/'
 });
 
 export default API;
