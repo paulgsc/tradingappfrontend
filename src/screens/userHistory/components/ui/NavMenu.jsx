b4733415bdd9f1cd6b854d4102b37460805cbcf6
Paul Gathondu <aulgondu@gmail.com>
2023-11-03 04:33:40 -0700

    css/html fix, text inplace of carot down arrow, banner notication z-index

--- src/screens/userHistory/components/ui/NavMenu.jsx
+++ src/screens/userHistory/components/ui/NavMenu.jsx
@@ -8,14 +8,16 @@ function NavMenu() {
         <div key={menu.id} className="group relative">
           <button
             aria-selected={location.pathname.includes(menu?.type)}
-            className="h-16 inline-flex items-center px-6 border-2 rounded-t-xl border-transparent hover:border-stone-400/20 group-focus-within:border-stone-400/20 aria-selected:relative  aria-selected:before:absolute aria-selected:before:bottom-0 aria-selected:before:left-0 before:translate-y-full aria-selected:before:w-3/5 aria-selected:before:h-1 aria-selected:before:rounded-full aria-selected:before:bg-blue-600/60"
+            className="h-16 inline-flex items-center gap-1 px-6 border-2 rounded-t-xl border-transparent hover:border-stone-400/20 group-focus-within:border-stone-400/20 aria-selected:relative  aria-selected:before:absolute aria-selected:before:bottom-0 aria-selected:before:left-0 before:translate-y-full aria-selected:before:w-3/5 aria-selected:before:h-1 aria-selected:before:rounded-full aria-selected:before:bg-blue-600/60"
             type="button"
             aria-haspopup="true"
             aria-expanded="true"
             aria-controls={`headlessui-menu-items-${menu.id}`}
           >
             <span className="relative z-10">{menu.title}</span>
-            {menu?.content && "&#x25BC;"}
+            {menu?.content && (
+              <i className="fas fa-caret-down fa-xs text-gray-600/60 group-focus-within:text-gray-600" />
+            )}
           </button>
 
           {menu?.content && <NavMenuItems dropdown={menu.content} />}
