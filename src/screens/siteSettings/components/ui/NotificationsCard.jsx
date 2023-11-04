d49fb653efdf0d40c618eb24bc798110eb95d0ff
Paul Gathondu <aulgondu@gmail.com>
2023-11-04 02:31:13 -0700

    css edits

---
+++ src/screens/siteSettings/components/ui/NotificationsCard.jsx
@@ -0,0 +1,23 @@
+import ToggleButton from "../../../../components/ui/ToggleButton";
+
+function NotificationsCard() {
+  return (
+    <ul className="w-full flex flex-col items-center mx-auto mt-12">
+      {Array(12)
+        .fill()
+        .map((item, i) => (
+          <li
+            key={i}
+            className="w-8/12 inline-flex items-center justify-between"
+          >
+            <span role="text" className="">
+              notifications
+            </span>
+            <ToggleButton />
+          </li>
+        ))}
+    </ul>
+  );
+}
+
+export default NotificationsCard;
