2254f8f7fb79dd7f456f4827f4e3791ba7eeb11b
Paul Gathondu <aulgondu@gmail.com>
2023-12-04 12:42:34 -0800

    fix: add missing alt attr on navbarlogo

--- src/components/navbar/navlogo/NavbarLogo.jsx
+++ src/components/navbar/navlogo/NavbarLogo.jsx
@@ -7,6 +7,10 @@ function NavbarLogo() {
       <Link to={"/"} className="">
         <div className="  flex items-center text-start justify-start rounded-lg shadow-sm h-12  w-36">
           <img
+            loading="lazy"
+            height={"48px"}
+            width={"144px"}
+            alt="website logo"
             src={siteLogo1}
             className="flex justify-start text-start object-left"
           />
