4e3c2154ead53f6135aaf4abb5945b1a522503c7
Paul Gathondu <aulgondu@gmail.com>
2023-12-04 10:15:27 -0800

    chore: add seo meta tags to home page

--- src/screens/home/routes/HomePage.jsx
+++ src/screens/home/routes/HomePage.jsx
@@ -1,4 +1,5 @@
 import EditWrapper from "../../../components/wrapper/EditWrapper";
+import Layout from "../Layout";
 import Footer from "../components/footer/Footer";
 import Hero from "../components/hero/Hero";
 import Testimonials from "../components/testimonials/Testimonials";
@@ -6,22 +7,24 @@ import Navbar from "../components/ui/Navbar";
 
 function HomePage() {
   return (
-    <div className="relative">
-      <section className="w-full sticky top-0 z-50 bg-white">
-        <Navbar />
-      </section>
-      <section className="xl:padding-l wide:padding-r padding-b">
-        <Hero />
-      </section>
-      <section className="w-full">
-        <EditWrapper path={"/admin/dashboard"}>
-          <Testimonials />
-        </EditWrapper>
-      </section>
-      <section className="">
-        <Footer />
-      </section>
-    </div>
+    <Layout>
+      <div className="relative">
+        <section className="w-full sticky top-0 z-50 bg-white">
+          <Navbar />
+        </section>
+        <section className="xl:padding-l wide:padding-r padding-b">
+          <Hero />
+        </section>
+        <section className="w-full">
+          <EditWrapper path={"/admin/dashboard"}>
+            <Testimonials />
+          </EditWrapper>
+        </section>
+        <section className="">
+          <Footer />
+        </section>
+      </div>
+    </Layout>
   );
 }
 
