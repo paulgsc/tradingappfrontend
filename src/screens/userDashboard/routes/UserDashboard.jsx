7a55343777ae113411ffe7b4323261092b35e454
Paul Gathondu <aulgondu@gmail.com>
2024-01-04 16:15:50 -0800

    fix: remove redudant hook from userdashboard

--- src/screens/userDashboard/routes/UserDashboard.jsx
+++ src/screens/userDashboard/routes/UserDashboard.jsx
@@ -3,14 +3,12 @@ import { getUserBalance } from "../../tradingportal/components/hooks/reactQuery"
 import DashboardLayout from "../components/ui/DashboardLayout";
 import Navbar from "../components/ui/Navbar";
 import NavLayout from "../../../components/ui/NavLayout";
-import useCoinbaseExchangeToken from "../hooks/useCoinBaseExchangeToken";
 import { useLocation } from "react-router";
 import CoinbaseAuthCard from "../components/ui/CoinbaseAuthCard";
 
 function UserDashboard() {
   const location = useLocation();
   const { userInfo: { token } = {} } = useSelector((state) => state.userAuth);
-  useCoinbaseExchangeToken(token);
 
   const { isLoading: userBalanceLoading } = getUserBalance(token);
 
